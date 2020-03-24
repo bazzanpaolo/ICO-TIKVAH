c915c735338edf4cd48857415822a7ac56a5f28a
Bazzan Paolo <jacobswellfarm@yahoo.com>
2020-03-24 12:32:50 +0100

    Initial repository
    Initial Repository su github

--- src/contracts/TIKVAHToken.sol
+++ src/contracts/TIKVAHToken.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.5.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
 
 contract TIKVAHToken is ERC20Mintable, ERC20Pausable, ERC20Detailed {
     constructor() public ERC20Detailed("TIKVAH", "TKVH", 18) {
--- test/TIKVAHTokenCrowdsale.test.js
+++ test/TIKVAHTokenCrowdsale.test.js
@@ -1,3 +1,9 @@
+ const ether = (n) => {
+  return new web3.utils.BN(
+    web3.utils.toWei(n.toString(), 'ether')
+  )
+}
+
 const TIKVAHToken = artifacts.require('./TIKVAHToken')
 const TIKVAHTokenCrowdsale = artifacts.require('./TIKVAHTokenCrowdsale')
 
@@ -5,8 +11,10 @@ require('chai')
   .use(require('chai-as-promised'))
   .should()
 
-contract('TIKVAHTokenCrowdsale', ([_, wallet]) => {
+contract('TIKVAHTokenCrowdsale', ([_, wallet, investor1, investor2,]) => {
 let rate
+let token
+
 
   beforeEach(async () => {
     const name = 'TIKVAH'
@@ -19,11 +27,14 @@ let rate
    // Crowdsale config
     const rate = '500'
 
+
     crowdsale = await TIKVAHTokenCrowdsale.new(
       rate,
       wallet,
       token.address
     )
+    // Transfer token ownership to crowdsale
+     await token.transferOwnership(crowdsale.address);
   })
 
   describe('crowdsale', () => {
@@ -41,5 +52,12 @@ let rate
       const result = await crowdsale.wallet()
       result.should.equal(wallet)
     })
+
+  describe('accepting payments', () => {
+     it('should accept payments', async () => {
+       const value = ether(1)
+       await crowdsale.sendTransactio({value: value, from: investor1})
+     })
+  })
   })
 })
